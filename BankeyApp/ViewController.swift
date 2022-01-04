b67af50fdf02076ec61c545f4f41a6eb71d9f8f3
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2022-01-04 20:36:07 +0300

    Initial Commit

---
+++ BankeyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankeyApp
+//
+//  Created by Олег Федоров on 04.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
