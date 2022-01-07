4985741d41d3b0b0502d9a7d4bbf2265a09a9f90
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2022-01-07 21:17:46 +0300

    Add onboarding view controller

--- BankeyApp/AppDelegate.swift
+++ BankeyApp/AppDelegate.swift
@@ -20,7 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = LoginViewController()
+//        window?.rootViewController = LoginViewController()
+        window?.rootViewController = OnboardingContainerViewController()
         
         return true
     }
