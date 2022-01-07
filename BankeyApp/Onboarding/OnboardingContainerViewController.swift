569fcabe025e621e9ca1b668a282e1afe1967e36
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2022-01-07 22:39:47 +0300

    Adding the close button on the Onboarding screen.

--- BankeyApp/Onboarding/OnboardingContainerViewController.swift
+++ BankeyApp/Onboarding/OnboardingContainerViewController.swift
@@ -11,11 +11,8 @@ class OnboardingContainerViewController: UIViewController {
     
     let pageViewController: UIPageViewController
     var pages = [UIViewController]()
-    var currentVC: UIViewController {
-        didSet {
-            
-        }
-    }
+    var currentVC: UIViewController
+    let closeButton = UIButton(type: .system)
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         self.pageViewController = UIPageViewController(
@@ -53,6 +50,12 @@ class OnboardingContainerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setup()
+        style()
+        layout()
+    }
+    
+    private func setup() {
         view.backgroundColor = .systemPurple
         
         addChild(pageViewController)
@@ -82,6 +85,33 @@ class OnboardingContainerViewController: UIViewController {
                                               animated: false,
                                               completion: nil)
         currentVC = pages.first!
+    }
+    
+    private func style() {
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.setTitle("Close", for: [])
+        closeButton.addTarget(self,
+                              action: #selector(closeTapped),
+                              for: .primaryActionTriggered)
+        view.addSubview(closeButton)
+    }
+    
+    private func layout() {
+        // closeButton
+        NSLayoutConstraint.activate([
+            closeButton.topAnchor.constraint(
+                equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor,
+                multiplier: 2
+            ),
+            closeButton.leadingAnchor.constraint(
+                equalToSystemSpacingAfter: view.leadingAnchor,
+                multiplier: 2
+            )
+        ])
+    }
+    
+    @objc private func closeTapped() {
+        
     }
 }
 
