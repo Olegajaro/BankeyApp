495268aaac33236a324973fbc3f841d00994cfde
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2022-01-17 15:42:42 +0300

    refactor: General AppDelegate cleanup

--- BankeyApp/AppDelegate.swift
+++ BankeyApp/AppDelegate.swift
@@ -30,41 +30,52 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         loginViewController.delegate = self
         onboardingContainerViewController.delegate = self
         
-        let vc = mainViewController
-        vc.setStatusBar()
-        
-        UINavigationBar.appearance().isTranslucent = false
-        UINavigationBar.appearance().backgroundColor = appColor
-        
-        window?.rootViewController = vc
-    
+        displayLogin()
         return true
     }
     
 }
 
-extension AppDelegate: LoginViewContollerDelegate {
-    func didLogin() {
+extension AppDelegate {
+    private func displayLogin() {
+        setRootViewController(loginViewController)
+    }
+    
+    private func displaNextScreen() {
         if LocalState.hasOnBoarded {
+            prepMainView()
             setRootViewController(mainViewController)
         } else {
             setRootViewController(onboardingContainerViewController)
         }
+    }
+    
+    private func prepMainView() {
+        mainViewController.setStatusBar()
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().backgroundColor = appColor
+    }
+}
+
+extension AppDelegate: LoginViewContollerDelegate {
+    func didLogin() {
+        displaNextScreen()
     }
 }
 
 extension AppDelegate: OnboardingContainerViewControllerDelegate {
     func didFinishOnboarding() {
         LocalState.hasOnBoarded = true
+        prepMainView()
         setRootViewController(mainViewController)
     }
 }
 
-extension AppDelegate: LogoutDelegate {
-    func didLogout() {
-        setRootViewController(loginViewController)
-    }
-}
+//extension AppDelegate: LogoutDelegate {
+//    func didLogout() {
+//        setRootViewController(loginViewController)
+//    }
+//}
 
 extension AppDelegate {
     func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
